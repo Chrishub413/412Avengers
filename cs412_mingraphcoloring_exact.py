e90492ea2e18c63acc589100d603c657542a0c0d
Ryan West <westrm@dukes.jmu.edu>
2024-11-20 11:12:29 -0500

    Creating temp results list

--- cs412_mingraphcoloring_exact.py
+++ cs412_mingraphcoloring_exact.py
@@ -16,6 +16,44 @@ Minimum colors: 4
 Color assignment: [0, 1, 2, 3]
 """
 
+def greedy_coloring(G, V):
+        # Initialize all vertices as unassigned
+
+        result = [-1] *  V
+        
+        # Assign the first color to the first vertex
+        result[0] = 0
+
+        # A temporary array to store the colors of the adjacent vertices
+        available = [False] * V
+
+        print(G.keys())
+        print(result)
+        print(available)
+
+        # # Assign colors to remaining V-1 vertices
+        # for u in range(1, V):
+        #     # Mark colors of adjacent vertices as unavailable
+        #     for i in G.keys():
+        #         if result[i] != -1:
+        #             available[result[i]] = True
+
+        #     # Find the first available color
+        #     for color in range(V):
+        #         if not available[color]:
+        #             break
+
+        #     result[u] = color
+
+        #     # Reset the values back to false for the next iteration
+        #     for i in G.keys():
+        #         if result[i] != -1:
+        #             available[result[i]] = False
+
+        # # Print the result
+        # for u in range(V):
+        #     print(f"Vertex {u} --> Color {result[u]}")
+
 def main():
     E = int(input())
     graph = {}
@@ -24,11 +62,15 @@ def main():
     for _ in range(E):
         v1, v2 = input().strip().split()
 
-        # use set for edges {'hburg': {'jmu'}, 'jmu':{'hburg', 'cville'}}
+        # use set for edges {'a': {'b'}, 'b':{'a', 'c'}, 'c':{'b'}}
         graph.setdefault(v1, set()).add(v2)
         graph.setdefault(v2, set()).add(v1)
 
-    print(graph)
+    V = len(graph)
+
+    ## Do coloring method
+
+    greedy_coloring(graph, V)
 
 if __name__ == "__main__":
     main()
