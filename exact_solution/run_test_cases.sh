ec04deb71bf0c8497a2a788563ad9fa576143d82
Chrishub413 <146030318+Chrishub413@users.noreply.github.com>
2024-12-05 23:59:07 -0500

    20 min version

--- exact_solution/run_test_cases.sh
+++ exact_solution/run_test_cases.sh
@@ -2,7 +2,7 @@ echo "Running graph coloring on all test cases..."
 
 python3 cs412_mingraphcolor_exact.py test_cases/graph_small.txt
 python3 cs412_mingraphcolor_exact.py test_cases/graph_medium.txt
-
+#20 mins version
 python3 cs412_mingraphcolor_exact.py test_cases/graph_large.txt
 
 echo "All test cases executed."
